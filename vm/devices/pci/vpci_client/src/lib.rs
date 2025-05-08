106c9a51bd0d02d61558c557412854f05b5dff61
John Starks <jostarks@microsoft.com>
2025-05-08 00:13:52 +0000

    wip

--- vm/devices/pci/vpci_client/src/lib.rs
+++ vm/devices/pci/vpci_client/src/lib.rs
@@ -3,44 +3,77 @@
 mod tests;
 
 use anyhow::Context;
-use vmbus_async::async_dgram::AsyncRecvExt;
-use vmbus_async::async_dgram::AsyncSendExt;
-use vmbus_async::pipe::MessagePipe;
+use guestmem::MemoryRead;
+use vmbus_async::queue::IncomingPacket;
+use vmbus_async::queue::OutgoingPacket;
+use vmbus_async::queue::Queue;
+use vmbus_channel::RawAsyncChannel;
 use vmbus_ring::RingMem;
 use vpci_protocol::MessageType;
 use vpci_protocol::ProtocolVersion;
+use vpci_protocol::QueryBusRelations2;
 use vpci_protocol::QueryProtocolVersion;
 use vpci_protocol::QueryProtocolVersionReply;
 use vpci_protocol::Status;
 use zerocopy::FromBytes;
+use zerocopy::Immutable;
 use zerocopy::IntoBytes;
+use zerocopy::KnownLayout;
 
-pub struct VpciClient<R: RingMem> {
-    pipe: VpciPipe<R>,
+pub struct VpciClient<M: RingMem> {
+    conn: VpciConnection<M>,
     protocol_version: ProtocolVersion,
 }
 
-struct VpciPipe<R: RingMem> {
-    pipe: MessagePipe<R>,
+struct VpciConnection<M: RingMem> {
+    queue: Queue<M>,
     buf: Vec<u8>,
 }
 
-impl<R: RingMem> VpciPipe<R> {
-    async fn recv(&mut self) -> anyhow::Result<&[u8]> {
-        let bytes = self.pipe.recv(&mut self.buf).await?;
-        Ok(&self.buf[..bytes])
+impl<M: RingMem> VpciConnection<M> {
+    async fn send<S: IntoBytes + Immutable>(&mut self, send: S) -> anyhow::Result<()> {
+        let mut write = self.queue.split().1;
+        write
+            .write(OutgoingPacket {
+                transaction_id: 0,
+                packet_type: vmbus_ring::OutgoingPacketType::InBandNoCompletion,
+                payload: &[send.as_bytes()],
+            })
+            .await
+            .context("failed to send protocol version query")?;
+        Ok(())
     }
 
-    async fn recv_prefix<T: FromBytes>(&mut self) -> anyhow::Result<T> {
-        let bytes = self.recv().await?;
-        Ok(T::read_from_prefix(bytes)
-            .ok()
-            .context("failed to parse message")?
-            .0)
+    async fn transact<
+        S: IntoBytes + Immutable,
+        R: FromBytes + IntoBytes + Immutable + KnownLayout,
+    >(
+        &mut self,
+        send: S,
+    ) -> anyhow::Result<R> {
+        let (mut read, mut write) = self.queue.split();
+        write
+            .write(OutgoingPacket {
+                transaction_id: 0,
+                packet_type: vmbus_ring::OutgoingPacketType::InBandWithCompletion,
+                payload: &[send.as_bytes()],
+            })
+            .await
+            .context("failed to send protocol version query")?;
+
+        let reply = read
+            .read()
+            .await
+            .context("failed to read protocol version reply")?;
+        let IncomingPacket::Completion(p) = &*reply else {
+            anyhow::bail!("unexpected packet type")
+        };
+        let reply = p.reader().read_plain()?;
+        Ok(reply)
     }
 }
 
-async fn negotiate<R: RingMem>(pipe: &mut VpciPipe<R>) -> anyhow::Result<ProtocolVersion> {
+async fn negotiate<M: RingMem>(conn: &mut VpciConnection<M>) -> anyhow::Result<ProtocolVersion> {
     // Try to negotiate versions in order from newest to oldest
     let versions = &[ProtocolVersion::VB];
 
@@ -53,17 +86,10 @@ async fn negotiate<R: RingMem>(pipe: &mut VpciPipe<R>) -> anyhow::Result<Protoco
             protocol_version: version,
         };
 
-        // Send the query and wait for response
-        pipe.pipe
-            .send(query.as_bytes())
+        let reply = conn
+            .transact::<_, QueryProtocolVersionReply>(query)
             .await
             .context("failed to send protocol version query")?;
-
-        let reply: QueryProtocolVersionReply = pipe
-            .recv_prefix()
-            .await
-            .context("failed to receive protocol version reply")?;
-
         if reply.status == Status::SUCCESS {
             return Ok(version);
         }
@@ -72,25 +98,49 @@ async fn negotiate<R: RingMem>(pipe: &mut VpciPipe<R>) -> anyhow::Result<Protoco
     anyhow::bail!("no supported VPCI protocol version found");
 }
 
-impl<R: RingMem> VpciClient<R> {
-    pub async fn connect(pipe: MessagePipe<R>) -> anyhow::Result<Self> {
-        let mut pipe = VpciPipe {
-            pipe,
+impl<M: RingMem> VpciClient<M> {
+    pub async fn connect(channel: RawAsyncChannel<M>) -> anyhow::Result<Self> {
+        let mut conn = VpciConnection {
+            queue: Queue::new(channel)?,
             buf: vec![0; vpci_protocol::MAXIMUM_PACKET_SIZE],
         };
 
-        let version = negotiate(&mut pipe)
+        let version = negotiate(&mut conn)
             .await
             .context("failed to negotiate protocol version")?;
 
-        Ok(Self {
-            pipe,
+        let mut this = Self {
+            conn,
             protocol_version: version,
-        })
+        };
+
+        this.conn
+            .send(MessageType::QUERY_BUS_RELATIONS)
+            .await
+            .context("failed to send bus relations query")?;
+
+        Ok(this)
     }
 
-    /// Returns the negotiated protocol version
-    pub fn protocol_version(&self) -> ProtocolVersion {
-        self.protocol_version
+    async fn run(&mut self) {
+        loop {
+            let read = self.conn.queue.split().0;
+            let packet = read
+                .read()
+                .await
+                .context("failed to read protocol version reply")
+                .unwrap();
+            let IncomingPacket::Data(p) = &*packet else {
+                tracing::warn!("unexpected packet type");
+                continue;
+            };
+
+            let reply = p.reader().read_plain().unwrap();
+            if reply.status == Status::SUCCESS {
+                tracing::info!("successfully negotiated protocol version");
+            } else {
+                tracing::warn!("failed to negotiate protocol version");
+            }
+        }
     }
 }
--- vm/devices/pci/vpci_client/src/tests.rs
+++ vm/devices/pci/vpci_client/src/tests.rs
@@ -8,15 +8,29 @@ use chipset_device::pci::PciConfigSpace;
 use closeable_mutex::CloseableMutex;
 use guestmem::GuestMemory;
 use guid::Guid;
+use mesh::rpc::RpcSend;
 use pal_async::DefaultDriver;
 use pal_async::async_test;
+use pal_async::task::Spawn;
 use parking_lot::Mutex;
 use std::sync::Arc;
+use std::task::Context;
+use task_control::StopTask;
 use test_with_tracing::test;
+use vmbus_async::pipe::MessagePipe;
+use vmbus_channel::SignalVmbusChannel;
 use vmbus_channel::bus::OfferInput;
 use vmbus_channel::bus::OpenData;
 use vmbus_channel::bus::OpenRequest;
 use vmbus_channel::bus::ParentBus;
+use vmbus_channel::simple::SimpleVmbusDevice;
+use vmbus_core::protocol::GpadlId;
+use vmbus_core::protocol::UserDefinedData;
+use vmbus_ring::IncomingRing;
+use vmcore::interrupt::Interrupt;
+use vmcore::notify::Notify;
+use vmcore::notify::PolledNotify;
+use vmcore::slim_event::SlimEvent;
 use vmcore::vm_task::SingleDriverBackend;
 use vmcore::vm_task::VmTaskDriverSource;
 use vpci::test_helpers::TestVpciInterruptController;
@@ -40,64 +54,27 @@ impl PciConfigSpace for NoopDevice {
     }
 }
 
-#[derive(Clone, Default)]
-struct VmbusOfferee(Arc<Mutex<Option<OfferInput>>>);
-
-#[async_trait]
-impl ParentBus for VmbusOfferee {
-    /// Offers a new channel.
-    async fn add_child(
-        &self,
-        request: vmbus_channel::bus::OfferInput,
-    ) -> anyhow::Result<vmbus_channel::bus::OfferResources> {
-        let mut this = self.0.lock();
-        if this.is_some() {
-            return Err(anyhow::anyhow!("Already offered"));
-        }
-        *this = Some(request);
-        Ok(vmbus_channel::bus::OfferResources::new(
-            GuestMemory::empty(),
-            None,
-        ))
-    }
-
-    fn clone_bus(&self) -> Box<dyn ParentBus> {
-        Box::new(self.clone())
-    }
-}
-
-//#[async_test]
+#[async_test]
 async fn test_negotiate_version(driver: DefaultDriver) {
-    let driver_source = VmTaskDriverSource::new(SingleDriverBackend::new(driver.clone()));
     let device = Arc::new(CloseableMutex::new(NoopDevice));
-    let vmbus = VmbusOfferee::default();
     let msi_controller = TestVpciInterruptController::new();
-    let server = vpci::bus::VpciBus::new(
-        &driver_source,
+    let (bus, mut channel) = vpci::bus::VpciBusDevice::new(
         Guid::new_random(),
         device,
         &mut ExternallyManagedMmioIntercepts,
-        &vmbus,
         msi_controller,
     )
-    .await
     .unwrap();
 
-    let params = vmbus.0.lock().take().unwrap();
-    params
-        .request_send
-        .send(vmbus_channel::bus::ChannelRequest::Open(OpenRequest {
-            open_data: OpenData {
-                target_vp: 0,
-                ring_offset: 10,
-                ring_gpadl_id: 0,
-                event_flag: b,
-                connection_id: todo!(),
-                user_data: todo!(),
-            },
-            interrupt: a,
-            use_confidential_ring: false,
-            use_confidential_external_memory: false,
-        }))
-        .await;
+    let (host, guest) = vmbus_channel::connected_async_channels(32768);
+
+    let mut runner = channel.open(host, GuestMemory::empty()).unwrap();
+    let _task = driver.spawn("server", async move {
+        StopTask::run_with(std::future::pending(), async |stop| {
+            let _ = channel.run(stop, &mut runner).await;
+        })
+        .await
+    });
+
+    super::VpciClient::connect(guest).await.unwrap();
 }
